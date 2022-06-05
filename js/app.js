931e07bd6fbe55aa43f58ced1f75af7dfe43db05
kevcb1988 <kevcb1988@gmail.com>
2022-06-05 18:16:41 -0500

    Desarrollo de funcionalidades: Agregar, Eliminar, Sumar cantidades, Vaciar Carrito

--- js/app.js
+++ js/app.js
@@ -1,30 +1,57 @@
 //Caprturo mis elementos fde HTML con variables
-const carritoCompras = document.querySelector("#contenido-carrito-de-compras");
+const carritoCompras = document.querySelector("#contenido-carrito-compras");
 const contenidoCarritoCompras = document.querySelector("#lista-carrito-compras tbody");
 const listaLibros = document.querySelector("#lista-libros");
 const limpiarCarrito = document.querySelector("#limpiar-carrito");
 let librosSeleccionados = [];
 
 
-//FUNCIONES
 cargarEventos();
-function cargarEventos(){
+//FUNCIONES
+function cargarEventos() {
     listaLibros.addEventListener('click', agregarLibro);
+    //Eliminar libros del carrito
+    carritoCompras.addEventListener('click', eliminarLibro);
+    //Vaciar carrito de compras
+    limpiarCarrito.addEventListener('click', vaciarCarrito);
 }
 
-function agregarLibro(e){
+
+//Agregar libor al carrito de compras
+function agregarLibro(e) {
     e.preventDefault();
-    if(e.target.classList.contains("agregar-libro")){
+    if (e.target.classList.contains("agregar-libro")) {
         //Selecciona la card principal de cada libro y la envío por parámetro
         const libroSeleccionado = e.target.parentElement.parentElement.parentElement;
         leerDatosLibro(libroSeleccionado)
     }
 }
 
-function leerDatosLibro(libro){
+//Eliminar libro del carrito de compras
+function eliminarLibro(e){
+    console.log(e.target.classList)
+    if(e.target.classList.contains('eliminar-libro')){
+        //Capturo el ID del libro
+        const libroID = e.target.getAttribute('data-id');
+        //Eliminar libro
+        librosSeleccionados = librosSeleccionados.filter(libro => libro.id !== libroID);
+        console.log(librosSeleccionados);
+        //Ejecuto la funcion carritoHTML, para recorrer el carrito y actualizarlo.
+        carritoHTML()
+    }
+}
+
+//Limpiar carrito de compras
+function vaciarCarrito(){
+    librosSeleccionados = [];
+    limpiarElementosCarrito();
+    console.log(librosSeleccionados)
+}
+
+function leerDatosLibro(libro) {
     console.log(libro)
     //Creo un objeto con los datos obtenidos desde agregarLibro()
-    const datosLibro = 
+    const datosLibro =
     {
         id: libro.querySelector('button').getAttribute('data-id'),
         portada: libro.querySelector('img').src,
@@ -34,30 +61,51 @@ function leerDatosLibro(libro){
         cantidad: 1
     }
 
-    //Agregar libros al array librosSeleccionados
-    //Saco una copia de mi arreglos de libroSeleccionados y le paso la información almacenada en datosLibro
-    librosSeleccionados = [...librosSeleccionados, datosLibro]
-    console.log(librosSeleccionados)
+    //Reviso si ya exite ul mimo libro dentro del carrito
+    const libro_existente = librosSeleccionados.some(libro => libro.id === datosLibro.id);
+    console.log(libro_existente)
+    if (libro_existente) {
+        //Actualizo la cantidad dentro del carrito
+        const libros = librosSeleccionados.map( libro => {
+            if(libro.id === datosLibro.id){
+                libro.cantidad ++;
+                return libro;
+            }else{
+                return libro;
+            }
+        });
+        librosSeleccionados = [...librosSeleccionados]
 
+    } else {
+        //Agrego libro al carrito
+        //Agregar libros al array librosSeleccionados
+        //Saco una copia de mi arreglos de libroSeleccionados y le paso la información almacenada en datosLibro
+        librosSeleccionados = [...librosSeleccionados, datosLibro]
+        console.log(librosSeleccionados)
+    }
     //Llamo la función carritoHTML() para agregar los libros dentro del carrito de compras
     carritoHTML();
 }
 
 //Muestra el carrito de compras
-function carritoHTML(){
+function carritoHTML() {
     //Limpio el array para evitar que se dupliquen los datos
     limpiarElementosCarrito();
 
     librosSeleccionados.forEach(libro => {
         console.log(libro)
+
+        //Limpiamos nuestro objeto, con esto al momento de imprimir nuesto libro tenemos más limpio el código
+        const { portada, titulo, precio, cantidad, id } = libro;
+
         const libroCarrito = document.createElement('tr');
         libroCarrito.innerHTML = `
-            <td class="portada"><img src="${libro.portada}"/></td>
-            <td class="titulo">${libro.titulo}</td>
-            <td class="precio">${libro.precio}</td>
-            <td class="cantidad">${libro.cantidad}</td>
+            <td class="portada"><img src="${portada}"/></td>
+            <td class="titulo">${titulo}</td>
+            <td class="precio">${precio}</td>
+            <td class="cantidad">${cantidad}</td>
             <td>
-                <a href="" data-id="${libro.id}"><img class="eliminar-libro" src="../img/trash-black.svg"/></a>
+                <a href="#" class="eliminar-libro" data-id="${id}"></a>
             </td>
         `
         //Agrego el contenido al tbody
@@ -65,9 +113,9 @@ function carritoHTML(){
     })
 }
 
-function limpiarElementosCarrito(){
+function limpiarElementosCarrito() {
     //contenidoCarritoCompras.innerHTML = "";
-    while(contenidoCarritoCompras.firstChild){
+    while (contenidoCarritoCompras.firstChild) {
         contenidoCarritoCompras.removeChild(contenidoCarritoCompras.firstChild)
     }
 }
