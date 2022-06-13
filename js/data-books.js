7a7328c9906c17b934191058a2e0b060fcc7624f
kevcb1988 <kevcb1988@gmail.com>
2022-06-12 20:48:49 -0500

    Restableciendo

---
+++ js/data-books.js
@@ -0,0 +1,18 @@
+const stockLibros = [
+    {
+        id:1,
+        portada: "",
+        titulo: "Harry Potter y la piedra filosofal",
+        autor: "J.K Rowling",
+        precio: "46.000",
+        genero: "Fantasia"
+    },
+    {
+        id:2,
+        portada: "",
+        titulo: "Harry Potter y la piedra filosofal",
+        autor: "J.K Rowling",
+        precio: "48.000",
+        genero: "Fantasia"
+    }
+]
